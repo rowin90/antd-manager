5c36e80e8e9f488129645e464f417405311ddd83
Jerome <raoju1580@qq.com>
2018-09-10 09:19:59 +0800

    (feature add asset resource)

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Menu, Icon } from 'antd'
+import MenuConfig from './../../config/menuConfig'
+const SubMenu = Menu.SubMenu;
 
 export default class NavLeft extends React.Component{
     render(){
         return (
-            <div>NavLeft</div>
+            <div>
+                <div className="logo">
+                    <img src="../src/resource/assets/logo-ant.svg" alt=""/>
+                    <h1>antd Manager</h1>
+                </div>
+                <Menu  style={{ width: 256 }} mode="vertical">
+                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+                        <Menu.Item key="1">Option 1</Menu.Item>
+                        <Menu.Item key="2">Option 2</Menu.Item>
+                        <Menu.Item key="3">Option 2</Menu.Item>
+                        <Menu.Item key="4">Option 2</Menu.Item>
+                    </SubMenu>
+                </Menu>
+            </div>
         )
     }
 }
