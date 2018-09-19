bf07e9611d54478d2af2b2ed473a4de74e6c6a81
Jerome <raoju1580@qq.com>
2018-09-19 23:28:28 +0800

    渲染左侧菜单

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import { Menu, Icon } from 'antd'
 import MenuConfig from './../../config/menuConfig'
+import './index.less'
 const SubMenu = Menu.SubMenu;
 
 export default class NavLeft extends React.Component{
+    componentWillMount(){
+        const menuTreeNode = this.renderMenu(MenuConfig);
+
+        this.setState({
+            menuTreeNode
+        })
+    }
+    // 菜单渲染
+    renderMenu =(data)=>{
+        return data.map((item)=>{
+            if(item.children){
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        { this.renderMenu(item.children)}
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item title={item.title} key={item.key}>
+               {item.title}
+            </Menu.Item>
+        })
+    }
     render(){
         return (
             <div>
                 <div className="logo">
-                    <img src="../src/resource/assets/logo-ant.svg" alt=""/>
+                    <img src="/assets/logo-ant.svg" alt=""/>
                     <h1>antd Manager</h1>
                 </div>
-                <Menu  style={{ width: 256 }} mode="vertical">
-                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                        <Menu.Item key="1">Option 1</Menu.Item>
-                        <Menu.Item key="2">Option 2</Menu.Item>
-                        <Menu.Item key="3">Option 2</Menu.Item>
-                        <Menu.Item key="4">Option 2</Menu.Item>
-                    </SubMenu>
+                <Menu 
+                    theme="dark"
+                >
+                    { this.state.menuTreeNode }
                 </Menu>
             </div>
         )
