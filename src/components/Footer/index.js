f1175178c92d5e8e4456095d3615fb17863cb973
Jerome <raoju1580@qq.com>
2018-09-21 11:57:22 +0800

    布局，中间main部分布局

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-
-export default class Footer extends React.Component{
-    render(){
+import React from 'react'
+import './index.less'
+export default class Footer extends React.Component {
+    render() {
         return (
-            <div>Footer</div>
-        )
+            <div className="footer">
+               版权所有：（推荐使用谷歌浏览器，可以获得更佳操作页面体验）
+            </div>
+        );
     }
 }
