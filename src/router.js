230a8dd70ee8c8230d4f56cf1152fc63c9f34f1e
Jerome <raoju1580@qq.com>
2018-09-27 23:46:52 +0800

    antd ui组件完成

--- src/router.js
+++ src/router.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import Login from './pages/login'
-import Buttons from './pages/ui/button'
+import Buttons from './pages/ui/buttons'
+import Modals from './pages/ui/modals'
 import NoMatch from './pages/nomatch/index';
+import Loadings from './pages/ui/loadings'
+import Notice from './pages/ui/notice'
+import Messages from './pages/ui/messages'
+import Tabs from './pages/ui/tabs'
+import Gallery from './pages/ui/gallery'
+import Carousel from './pages/ui/carousel'
+import FormLogin from './pages/form/login'
 import Admin from './admin'
 import App from './App'
 
@@ -16,6 +24,14 @@ export default class IRouter extends React.Component{
                         <Admin>
                             <Switch>
                                 <Route path="/admin/ui/buttons" component={Buttons}/>
+                                <Route path="/admin/ui/modals" component={Modals}/>
+                                <Route path="/admin/ui/loadings" component={Loadings} />
+                                <Route path="/admin/ui/notification" component={Notice} />
+                                <Route path="/admin/ui/messages" component={Messages} />
+                                <Route path="/admin/ui/tabs" component={Tabs} />
+                                <Route path="/admin/ui/gallery" component={Gallery} />
+                                <Route path="/admin/ui/carousel" component={Carousel} />
+                                <Route path="/admin/form/login" component={FormLogin} />
                                 <Route component={NoMatch}/>
                             </Switch>
                         </Admin>
