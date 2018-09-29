c3c614d67de9d2e8a56ce0eb48e54d8846e055fd
Jerome <raoju1580@qq.com>
2018-09-29 21:07:58 +0800

    注册表单组件完成

--- src/router.js
+++ src/router.js
@@ -11,6 +11,7 @@ import Tabs from './pages/ui/tabs'
 import Gallery from './pages/ui/gallery'
 import Carousel from './pages/ui/carousel'
 import FormLogin from './pages/form/login'
+import FormRegister from './pages/form/register'
 import Admin from './admin'
 import App from './App'
 
@@ -32,6 +33,7 @@ export default class IRouter extends React.Component{
                                 <Route path="/admin/ui/gallery" component={Gallery} />
                                 <Route path="/admin/ui/carousel" component={Carousel} />
                                 <Route path="/admin/form/login" component={FormLogin} />
+                                <Route path="/admin/form/reg" component={FormRegister} />
                                 <Route component={NoMatch}/>
                             </Switch>
                         </Admin>
