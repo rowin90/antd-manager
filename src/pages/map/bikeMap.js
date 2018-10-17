0351faea5f982c93befa3c926ac692832eb1a2ba
Jerome <raoju1580@qq.com>
2018-10-18 00:32:34 +0800

    完成车辆地图，路线，车辆分布功能

--- src/pages/map/bikeMap.js
+++ src/pages/map/bikeMap.js
@@ -65,6 +65,67 @@ export default class BikeMap extends React.Component{
         let endPoint = new window.BMap.Point(gps2[0],gps2[1])
         // 缩放地图，11级
         this.map.centerAndZoom(endPoint,11)
+
+        // 创建车辆起点（路径，坐标图片大小，坐标空间大小）
+        let startPointIcon = new window.BMap.Icon('/assets/start_point.png',new window.BMap.Size(36,42),{
+            imageSize:new window.BMap.Size(36,42),
+            // 图片偏移量
+            anchor:new window.BMap.Size(18,42)
+        })
+        // 车辆起点，覆盖到地图上
+        let bikeMarkerStart = new window.BMap.Marker(startPoint,{icon:startPointIcon})
+        this.map.addOverlay(bikeMarkerStart);
+      
+        let endPointIcon = new window.BMap.Icon('/assets/end_point.png',new window.BMap.Size(36,42),{
+            imageSize:new window.BMap.Size(36,42),
+            anchor:new window.BMap.Size(18,42)
+        })
+        let bikeMarkerEnd = new window.BMap.Marker(endPoint,{icon:endPointIcon})
+        this.map.addOverlay(bikeMarkerEnd);
+
+        // 绘制车辆行驶路线
+        let routeList = [];
+        list.forEach((item)=>{
+            let p = item.split(',');
+            // 添加百度地图坐标点
+            routeList.push(new window.BMap.Point(p[0],p[1]))
+        })
+
+        let polyLine = new window.BMap.Polyline(routeList,{
+            strokeColor:'#ef6136',
+            strokeWidth:2,
+            strokeOpacity:1
+        })
+        this.map.addOverlay(polyLine);
+
+        // 绘制服务区
+        let servicePointList = [];
+        let serviceList = res.result.service_list;
+        serviceList.forEach(item =>{
+            servicePointList.push(new window.BMap.Point(item.lon,item.lat))
+        })
+        let polyServiceLine = new window.BMap.Polyline(servicePointList,{
+            strokeColor:'#ef6136',
+            strokeWidth:3,
+            strokeOpacity:1
+        })
+        this.map.addOverlay(polyServiceLine);
+
+        // 添加地图中的自行车图标
+        let bikeList = res.result.bike_list;
+
+        let bikeIcon = new window.BMap.Icon('/assets/bike.jpg',new window.BMap.Size(36,42),{
+            imageSize:new window.BMap.Size(36,42),
+            // 图片偏移量
+            anchor:new window.BMap.Size(18,42)
+        })
+        bikeList.forEach(item =>{
+            let p = item.split(',');
+            let point = new window.BMap.Point(p[0],p[1])
+            let bikeMarker = new window.BMap.Marker(point,{icon:bikeIcon})
+            this.map.addOverlay(bikeMarker);
+        })
+
     }
 
     render(){
