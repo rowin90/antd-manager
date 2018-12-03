e699a7e4be1801b8d94b7785de8fd1fbabd5dc30
raoju <raoju1580@qq.com>
2018-12-03 23:29:52 +0800

    修改文件

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('welcome zhang bing bing !');
+    res.end('welcome zhang bing bing 2222222!');
 }).listen(3000)
 
 console.log('run at 3000');
